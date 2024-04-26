fd37f1de1bb1476b69105e6d16460fa97f2c505b
c3625505 <j.torbitt2295@student.leedsbeckett.ac.uk>
2024-04-26 13:39:31 +0000

    added redirect route for SQLInjection vulnerability if a valid query is not entered

--- resources/views/vulnerabilities/SQLInjection/explainSQLInjection.blade.php
+++ resources/views/vulnerabilities/SQLInjection/explainSQLInjection.blade.php
@@ -6,9 +6,11 @@
         @yield('content')
         <h1><u>What is SQL Injection</u></h1>
         <br>
-        <p> SQL Injection is..... </p>
+        <p>An SQL Injection attack occurs when an SQL query is injected or inserted via the input field from the client side to the application.  This can be done by inputting a malicious payload into an input field, such as the search bar. </p>
+
+        <p>Once inserted, the exploit is able to read sensitive data within the database, alter database data and perform administrative actions, for example, deleting users or even shutting down the database management system </p>
     </main>
-    
+   
     <script src="/js/app.js"></script>
 </body>
 
